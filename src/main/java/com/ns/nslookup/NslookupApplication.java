ef6d262240dea8accd7ec1fc972c2c6bbbbd3067
Nagaraj Sunagar <nagarajsunagar2002@gmail.com>
2024-10-20 16:42:05 +0530

    Initial Commit

---
+++ src/main/java/com/ns/nslookup/NslookupApplication.java
@@ -0,0 +1,13 @@
+package com.ns.nslookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NslookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NslookupApplication.class, args);
+	}
+
+}
